02804d95c69251f2e818b96352e4504680ffb61d
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-10-26 20:21:14 +0800

    feat(pages): base template and page template

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, redirect, url_for
 from image_processing import generate_frames
 from config import BROWSER_WS_PORT
 
@@ -6,7 +6,16 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    return render_template("index.html", ws_port=BROWSER_WS_PORT)
+    is_setup = True   # if prototype is setup properly
+    return render_template("index.html", is_setup=is_setup)
+
+@app.route("/admin")
+def admin_page():
+    return render_template("admin.html", ws_port=BROWSER_WS_PORT)
+
+@app.route("/client")
+def client_page():
+    return render_template("client.html", ws_port=BROWSER_WS_PORT)
 
 @app.route("/video_feed")
 def video_feed():
@@ -14,4 +23,12 @@ def video_feed():
 
 def run_webserver(port):
     print(f"[FLASK] running on 0.0.0.0:{port}")
-    app.run(host="0.0.0.0", port=port, threaded=True)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True,   # ✅ enables development mode
+        threaded=True
+    )
+
+if __name__ == "__main__":
+    run_webserver(5000)
